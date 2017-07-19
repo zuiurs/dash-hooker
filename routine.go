54c9cf2adddfb599f0469b2e7476f15feef751bc
zuiurs <z11111001011@gmail.com>
2017-07-19 15:10:54 +0900

    add sample code of slack webhook

--- routine.go
+++ routine.go
@@ -1,10 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+var (
+	WebhookEP string = "https://hooks.slack.com/services/XXXXXXXXX/YYYYYYYYY/ZZZZZZZZZZZZZZZZZZZZZZZZ"
 )
 
 // write your code
 func routine() {
-	fmt.Println("Dash Button is pushed")
+	type Payload struct {
+		Channel   string `json:"channel"`
+		Username  string `json:"username"`
+		IconEmoji string `json:"icon_emoji`
+		Text      string `json:"text"`
+	}
+
+	p := Payload{
+		Channel:   "#test",
+		Username:  "DashBot",
+		IconEmoji: ":runner:",
+		Text:      "DashButton is pushed",
+	}
+
+	b, err := json.Marshal(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	values := url.Values{}
+	values.Add("payload", string(b))
+
+	_, err = http.PostForm(WebhookEP, values)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
